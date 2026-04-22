ba469e869f282d2fd26936a97d402974f8dbfe08
berryz2012 <128698346+berryz2012@users.noreply.github.com>
2026-04-22 15:11:22 +0800

    Delete test-store.ts

--- test-store.ts
+++
@@ -1,6 +0,0 @@
-import { useStore } from './src/store/useStore';
-const state = useStore.getState();
-console.log('Init state:', state.apiKeys);
-setTimeout(() => {
-    console.log('Hydrated state:', useStore.getState().apiKeys);
-}, 2000);
