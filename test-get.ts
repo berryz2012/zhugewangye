2dfb670909d5f6f01750295e5ed66d0db2d7ae5a
berryz2012 <128698346+berryz2012@users.noreply.github.com>
2026-04-22 15:10:54 +0800

    Delete test-get.ts

--- test-get.ts
+++
@@ -1,6 +0,0 @@
-const testGet = async () => {
-    const res = await fetch('http://localhost:3000/api/config?name=ai-short-drama-storage');
-    const data = await res.json();
-    console.log(data);
-};
-testGet();
