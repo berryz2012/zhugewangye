2b1cff7d68d5ae2eb75d400adfecb4d0368b8001
berryz2012 <128698346+berryz2012@users.noreply.github.com>
2026-04-22 15:11:03 +0800

    Delete test-node-get.ts

--- test-node-get.ts
+++
@@ -1,7 +0,0 @@
-import http from 'http';
-
-http.get('http://localhost:3000/api/config?name=ai-short-drama-storage', (res) => {
-    let data = '';
-    res.on('data', chunk => data += chunk);
-    res.on('end', () => console.log('GET result:', data));
-});
